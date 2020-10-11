aee14f037b19c6f9c3508597e481825d3a953767
olivierargaud <olivier.argaud@ipilyon.net>
2020-10-11 09:42:45 +0200

    ajout du fichier randomDice.py

---
+++ randomDice.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/test')
+def test():
+    return 'premier test!'
