ad9d733636ecfb68a533fbe4d99efabb521d7699
olivierargaud <olivier.argaud@ipilyon.net>
2020-10-12 10:58:47 +0200

    avant sql

--- randomDice.py
+++ randomDice.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -10,6 +10,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
-
+    return render_template ('index.html')
 
